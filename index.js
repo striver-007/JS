b86361be1e4117b21a0c8a5f30d86e4b34d41e35
Vaibhav <singhvaibhav24052000@gmail.com>
2022-02-16 19:31:01 +0530

    till backticks

---
+++ index.js
@@ -0,0 +1,32 @@
+// Starting js -> Javascript
+// var let const
+
+//variable declaration
+var a; //int string float ??
+
+// variable initialization
+a=10;
+console.log(typeof a); //typeof ->which type of variable is a
+console.log(a); //console.log() is use to print in js
+
+a="hello world" //reinintializing
+console.log(typeof a);
+console.log(a);
+
+a =true;  //reinintializing
+console.log(typeof a);
+console.log(a);
+
+var str1 ="how r u??\nI m fine thanku mf"
+console.log(str1);
+
+
+// use cases of backticks ``
+var str2 =`how r u??
+I m fine thanku mf`
+console.log(str2);
+
+var num = 2400;
+console.log(`half of ${num} is ${num/2}`);
+
+
