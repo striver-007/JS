ee535d61372738368b02c53e00e74ae032185a4f
Vaibhav <singhvaibhav24052000@gmail.com>
2022-02-17 18:11:55 +0530

    1 function

---
+++ funtions.js
@@ -0,0 +1,23 @@
+//  3 types of functions are present
+
+
+
+//Normal function
+
+
+//function declaration
+// function function_name(parm1,parm2){
+
+//     //code
+
+// }
+
+//function invokeation
+// function_name(arg1,arg2);
+
+//simple function to add
+function add( a,b){
+    console.log(a+b);
+}
+
+add(6,9);
